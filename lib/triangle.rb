e6d3d5f1008055a06a6c36f41374f222082b1e06
Sierra McCahon <sierrakatemitchell7@gmail.com>
2019-01-14 23:12:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,9 +4,9 @@ class Triangle
   
   
   def initialize(side_one, side_two, side_three)
-    @side_one = 
-    @side_two
-    @side_three
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
   end 
 end
 ()
