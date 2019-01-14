604bb2421dd9f112b44ede3808a183436e1f9b20
Sierra McCahon <sierrakatemitchell7@gmail.com>
2019-01-14 23:26:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,7 @@ class Triangle
     if (@side_one + @side_two) <= @side_three || (@side_two + @side_three) <= @side_one || (@side_three + @side_one) <= @side_two 
     raise TriangleError
   else
+    
  end 
   
   end 
