d294ea72bddf954aee739832f294e7e455928695
Sierra McCahon <sierrakatemitchell7@gmail.com>
2019-01-14 23:29:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,7 @@ class Triangle
   def kind
     if (@side_one * @side_two * @side_three) == 0 || (@side_one + @side_two) <= @side_three || (@side_two + @side_three) <= @side_one || (@side_three + @side_one) <= @side_two 
     
+    begin
     raise TriangleError
   else
     
