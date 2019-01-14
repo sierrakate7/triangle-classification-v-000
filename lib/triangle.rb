479c61f2e803c2f88f288686da6abebea4188b91
Sierra McCahon <sierrakatemitchell7@gmail.com>
2019-01-14 23:23:55 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,20 @@ class Triangle
     @side_three = side_three
   end 
   
-  def kind 
-    if (@side_one + @side_two) <= @side_three || 
+  def kind
+    if valid?
+      if @triangle_sides.uniq.length == 1
+        return :equilateral
+      elsif @triangle_sides.uniq.length == 2
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+end
   
   end 
 end
